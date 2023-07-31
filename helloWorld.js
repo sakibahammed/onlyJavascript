b6deb04ccd7db118ab2c9e519ddd7d88bfb69ea0
sakibahammed <sakib.ahammed2345@gmail.com>
2023-07-31 17:53:53 +0600

    ionlyjs

---
+++ helloWorld.js
@@ -0,0 +1,27 @@
+console.log('hello world'); 
+// js is a high level , interpreted programming language used to make web pages more interactive , IT let people implement complex and beautiful thing design on web pages 
+
+// declare variable
+
+var sunglassPrice = 12;
+
+var specialOneName = 'samsung';
+
+
+// string type variable 
+
+let special = 'Araiana Grande';
+
+
+
+// boolean type variable
+
+var isSingle = true;
+
+
+
+// two types of number = Integer : 120;
+
+// float , decimal : 3.65; v
+
+
