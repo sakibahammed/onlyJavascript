d9a40b18cd01fafb96242f4c24aa963cf67ad69f
sakibahammed <sakib.ahammed2345@gmail.com>
2023-08-04 15:23:30 +0600

    filterfind.js

---
+++ filterFind.js
@@ -0,0 +1,10 @@
+const numbers = [5,13,4,47,7,29,79,30,5,2,19];
+
+const bugNumbers = numbers.filter(number=>number>20);
+// console.log(bugNumbers); // filter works like simply as map;
+
+
+console.log(numbers.find(number=>number>20));
+
+
+//find simpy loop chalabe and first e jeta paabe ota e return kore debe
